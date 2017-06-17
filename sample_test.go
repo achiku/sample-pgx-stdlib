aaa80a9c039502a538936bd1c1eee770069e4dca
Akira Chiku <akira.chiku@gmail.com>
2017-06-17 11:22:10 +0900

    Add AcquireConn sample

--- sample_test.go
+++ sample_test.go
@@ -71,3 +71,35 @@ func TestSetConConfig(t *testing.T) {
 	wg.Wait()
 	t.Logf("Final OpenConn=%d", db.Stats().OpenConnections)
 }
+
+func TestAcquireConn(t *testing.T) {
+	db := testNewDB(t)
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(50)
+	// db.SetConnMaxLifetime(time.Duration(1))
+
+	var (
+		tm time.Time
+		wg sync.WaitGroup
+	)
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		x := i
+		go func() {
+			conn, err := stdlib.AcquireConn(db)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer stdlib.ReleaseConn(db, conn)
+
+			if err := conn.QueryRow(`select now()`).Scan(&tm); err != nil {
+				t.Fatal(err)
+			}
+			t.Logf("%d: %s", x, tm)
+			t.Logf("OpenConn=%d", db.Stats().OpenConnections)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("Final OpenConn=%d", db.Stats().OpenConnections)
+}
