a44baebb84db6a3adebdd76d0a3b6e665b960bc8
Akira Chiku <akira.chiku@gmail.com>
2017-07-02 22:10:01 +0900

    Add date test

--- sample_test.go
+++ sample_test.go
@@ -115,12 +115,27 @@ func TestAcquireConnWithMaxLifetime(t *testing.T) {
 
 func testDBSetup(t *testing.T, db *sql.DB) func() {
 	db.Exec(`drop table t1`)
-	_, err := db.Exec(`create table t1 (id serial)`)
+	db.Exec(`drop table t2`)
+	_, err := db.Exec(`
+	create table t1 (
+		id serial
+	)`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = db.Exec(`
+	create table t2 (
+		id serial
+		,tm timestamp with time zone not null
+		,dt date
+	)
+	`)
 	if err != nil {
 		t.Fatal(err)
 	}
 	return func() {
 		db.Exec(`drop table t1`)
+		db.Exec(`drop table t2`)
 		db.Close()
 	}
 }
@@ -161,4 +176,29 @@ func TestTx(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("DBCount=%d", cnt3)
+}
+
+func TestTimeWithTz(t *testing.T) {
+	db := testNewDB(t)
+	f := testDBSetup(t, db)
+	defer f()
+
+	now := time.Now()
+	t.Logf("%s", now)
+	var ID int64
+	err := db.QueryRow(`insert into t2 (tm, dt) values ($1, $2) returning id`, now, now).Scan(&ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var (
+		tm time.Time
+		dt time.Time
+	)
+	err = db.QueryRow(`select tm, dt from t2 where id = $1`, ID).Scan(&tm, &dt)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("tm=%s", tm)
+	t.Logf("dt=%s", dt)
 }
